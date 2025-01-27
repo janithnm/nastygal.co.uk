fece59600353628a013c522bdb382fa9849a556e
JanithNimhara <137036759+janithnm@users.noreply.github.com>
2025-01-27 20:58:56 +0530

    2nd

--- clientaaa/src/components/MainLogo/MainLogo.jsx
+++
@@ -1,8 +0,0 @@
-import MainLogoImage from "../../assets/images/logo/logo.webp"
-export const MainLogo = () => {
-  return (
-    <div className="w-32 h-auto">
-        <img className="w-full h-full" src={MainLogoImage} alt="" srcset="" />
-    </div>
-  )
-}
