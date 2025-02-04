51cade94a282d45aed40637aa3ae452ba414e8f1
JanithNimhara <137036759+janithnm@users.noreply.github.com>
2025-02-04 19:59:06 +0530

    Update AppRouter.jsx

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -22,10 +22,6 @@ const router = createBrowserRouter([
             index:true,
             element: <Shop />,
           },
-          {
-            path: "shop",
-            element: <Home />,
-          },
         ],
       },
     ],
